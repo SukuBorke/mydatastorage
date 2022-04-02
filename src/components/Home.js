497a4547a1d4d4970a30e2303db3c6f11bc671e5
SukuBorke <saborke10@gmail.com>
2022-04-02 10:57:29 +0530

    Wowwww

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,8 @@ function Home() {
   return (<>
           <h1>This is changes done by another colabrator</h1>
     <div id='homenav'><span><Link className='elink1' to="/login">Login</Link><Link to="/register" className='elink1'>Create Account</Link></span></div>
+    <div></div>
+    
     </>
   )
 }
