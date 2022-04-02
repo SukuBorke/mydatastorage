90c2186373c825968199e16402bebc6751b4e1c4
SukuBorke <102648300+SukuBorke@users.noreply.github.com>
2022-04-02 10:50:06 +0530

    Another colabrator done changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (<>
+          <h1>This is changes done by another colabrator</h1>
     <div id='homenav'><span><Link className='elink1' to="/login">Login</Link><Link to="/register" className='elink1'>Create Account</Link></span></div>
     </>
   )
