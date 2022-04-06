c820f6430ec1db299e42b1ec6d45e7b16bf2c318
SukuBorke <saborke10@gmail.com>
2022-04-06 17:55:43 +0530

    Animated text added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (<>
-          <h1>This is changes done by Sukumar himself</h1>
+         
     <div id='homenav'><span><Link className='elink1' to="/login">Login</Link><Link to="/register" className='elink1'>Create Account</Link></span></div>
-    <div></div>
+    <div className="animated-text">
+      <div className="line">Do you want to save contacts</div>
+      <div className="line">You are in right place</div>
+      <div className="line">Register to our site</div>
+      <div className="line">Then please Login</div>
+    </div>
+
+   
     
     </>
   )
